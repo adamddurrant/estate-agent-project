d5dacfdca678b59b2fbff687fa4bf1702afdc465
Adam Durrant <adamdurrant92@gmail.com>
2023-01-02 11:28:36 +0000

    added username edit function which updates in firestore and auth

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,15 @@
-import { getAuth } from "firebase/auth";
+import { getAuth, updateProfile } from "firebase/auth";
+import { doc, updateDoc } from "firebase/firestore";
 import React, { useState } from "react";
 import { Navigate, useNavigate } from "react-router";
 import { toast } from "react-toastify";
+import { db } from "../firebase";
 
 export default function Profile() {
   const auth = getAuth();
   const navigate = useNavigate();
+  const [changeDetail, setChangeDetail] = useState(false);
+
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
@@ -17,6 +21,33 @@ export default function Profile() {
     toast.success("Signed out successfully ✌️");
     navigate("/");
   }
+  function onChange(e) {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  }
+
+  async function onSubmit() {
+    try {
+      if (auth.currentUser.displayName !== name) {
+        //update display name in firebase auth
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+        });
+
+        //update name in firestore db
+        const docRef = doc(db, "users", auth.currentUser.uid);
+        await updateDoc(docRef, {
+          name,
+        });
+      }
+      toast.success("Username updated!");
+    } catch (error) {
+      toast.error("could not update");
+    }
+  }
+
   return (
     <>
       <section className='max-w-6xl mx-auto flex justify-center items-center flex-col'>
@@ -28,8 +59,11 @@ export default function Profile() {
               type='text'
               id='name'
               value={name}
-              disabled
-              className='w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out mb-6'
+              disabled={!changeDetail}
+              onChange={onChange}
+              className={`w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out mb-6 ${
+                changeDetail && "bg-green-200 focus:bg-green-200"
+              }`}
             />
             {/* eEmail input */}
             <input
@@ -42,8 +76,14 @@ export default function Profile() {
             <div className='flex justify-between whitespace-nowrap text-sm sm:text-lg my-6'>
               <p className='flex items-center'>
                 Change username?{" "}
-                <span className='text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer'>
-                  Edit username
+                <span
+                  onClick={() => {
+                    changeDetail && onSubmit();
+                    setChangeDetail((prevState) => !prevState);
+                  }}
+                  className='text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer'
+                >
+                  {changeDetail ? "Apply change" : "Edit username"}
                 </span>
               </p>
               <p
